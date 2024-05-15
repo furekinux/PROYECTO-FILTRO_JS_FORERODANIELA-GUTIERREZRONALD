0897385cb0c7fbbeb08f6e0a44431a69e111d08a
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-05-15 09:03:50 -0500

    feat: :sparkles: Agregar actualizar información para Engines

--- js/main.js
+++ js/main.js
@@ -29,40 +29,81 @@ setTimeout(() => {
                             let rocket = JSON.parse(this.responseText)
                             console.log(rocket)
                             let rocketIn = document.getElementById("rocketInfo")
-                            if(rocket.landing_legs.material ===null){
+                            let engineIn = document.getElementById("engineInfo")
+                            let imgIn = document.getElementById("images")
+
+
+                            if(rocket.landing_legs.material === null){
                                 rocket.landing_legs.material = "none"
+                            }else{}
+                            if(rocket.active === false){
+                                rocket.active = "no"
+                            }else{
+                                rocket.active = "yes"
                             }
+                            let rocketN = document.getElementById("header__title").innerHTML = `${rocket.name}`
                             rocketIn.innerHTML = `
-                            <table>
+                            <h3>ROCKET INFORMATION</h3>
+                            <table class="table_R">
                                 <tr>
-                                    <th>Type</th><th class="color_w">${rocket.type}</th>
+                                    <th style="width: 9vw" class="color_v">Type</th><th class="color_w">${rocket.type}</th>
                                 </tr>
                                 <tr>
-                                    <th>Rocket in service</th><th class="color_w">${rocket.active}</th>
+                                    <th class="color_v">Rocket in service</th><th class="color_w">${rocket.active}</th>
                                 </tr>
                                 <tr>
-                                    <th>Number of stages</th><th class="color_w">${rocket.stages}</th>
+                                    <th class="color_v">Number of stages</th><th class="color_w">${rocket.stages}</th>
                                 </tr>
                                 <tr>
-                                    <th>Number of propellants</th><th class="color_w">${rocket.type}</th><!--Hello-->
+                                    <th class="color_v">Number of propellants</th><th class="color_w">${rocket.type}</th><!--Hello-->
                                 </tr>
                                 <tr>
-                                    <th>Landing legs</th><th class="color_w">${rocket.landing_legs.number}</th>
+                                    <th class="color_v">Landing legs</th><th class="color_w">${rocket.landing_legs.number}</th>
                                 </tr>
                                 <tr>
-                                    <th>Leg material</th><th class="color_w">${rocket.landing_legs.material}</th>
+                                    <th class="color_v">Leg material</th><th class="color_w">${rocket.landing_legs.material}</th>
                                 </tr>
                                 <tr>
-                                    <th>Height</th><th class="color_w">${rocket.height.meters} m</th>
+                                    <th class="color_v">Height</th><th class="color_w">${rocket.height.meters} m</th>
                                 </tr>
                                 <tr>
-                                    <th>Diameter</th><th class="color_w">${rocket.diameter.meters} m</th>
+                                    <th class="color_v">Diameter</th><th class="color_w">${rocket.diameter.meters} m</th>
                                 </tr>
                                 <tr>
-                                    <th>Mass</th><th class="color_w">${rocket.mass.kg} kg</th>
+                                    <th class="color_v">Mass</th><th class="color_w">${rocket.mass.kg} kg</th>
                                 </tr>
                             </table>
                             `
+                            engineIn.innerHTML = `
+                            <h3>ENGINE INFORMATION</h3>
+                            <table class="table_R">
+                                <tr>
+                                    <th style="width: 9vw" class="color_v">Type</th><th class="color_w">${rocket.engines.type}</th>
+                                </tr>
+                                <tr>
+                                    <th class="color_v">Maximum power loss</th><th class="color_w">${rocket.engines.engine_loss_max}</th>
+                                </tr>
+                                <tr>
+                                    <th class="color_v">Engine availability</th><th class="color_w">${rocket.engines.layout}</th>
+                                </tr>
+                                <tr>
+                                    <th class="color_v">Number of engines</th><th class="color_w">${rocket.engines.number}</th><!--Hello-->
+                                </tr>
+                                <tr>
+                                    <th class="color_v">Stage 1 fuel</th><th class="color_w">${rocket.engines.propellant_1}</th>
+                                </tr>
+                                <tr>
+                                    <th class="color_v">Stage 2 fuel</th><th class="color_w">${rocket.engines.propellant_2}</th>
+                                </tr>
+                            </table>
+                            `
+                            rocket.flickr_images.forEach(img => {
+                                newimg = document.createElement("img")
+                                newimg.innerHTML = `<img src="${img}"/>`
+                                imgIn.appendChild(newimg)
+                                
+                            });
+                            
                         } else if(this.readyState ===4){
                             console.log("Error :(",this.statusText)
                         }
