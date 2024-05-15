70035800f801b1f5144c00bfffe50c98f0872adc
Daniela Forero Ballén <156367697+furekinux@users.noreply.github.com>
2024-05-15 18:40:15 -0500

    feat: :sparkles: Agregar funciones para recorrer el api

---
+++ js/history.js
@@ -0,0 +1,48 @@
+setTimeout(() => {
+    arrayHistory= []
+    let items = document.querySelectorAll("div")
+    let items2 = document.querySelectorAll("section")
+    items.forEach(element => {
+        element.classList.remove("load")
+    });
+    items2.forEach(element => {
+        element.classList.remove("load")
+    });
+    items.forEach(element => {
+        element.classList.add("appear")
+    });
+    items2.forEach(element => {
+        element.classList.add("appear")
+    });
+    const apiUrlRockets = 'https://api.spacexdata.com/v4/history';
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET",apiUrlRockets,true);
+    xhr.onreadystatechange = function(){
+        if (this.readyState === 4 && this.status ===200){
+            let response = JSON.parse(this.responseText)
+            console.log(response)
+            i=1
+            fetchDefault(response)
+        } else if(this.readyState ===4){
+            console.log("Error :(",this.statusText)
+        }
+    }
+    xhr.send();
+}, 0);
+function fetchDefault(data){
+    data.forEach(element => {
+        let date=element.event_date_utc
+        let newdate=date.slice(0,10).replaceAll("-","/")
+        let newdiv = document.createElement("div");
+        newdiv.classList.add("square")
+        newdiv.innerHTML = `
+        <h2 class="color_w">${element.title}</h2>
+        <p class="color_v"><b>${newdate}</b></p>
+        <p class="color_v">${element.details}</p>
+        <a href="">LEARN MORE.</a>
+        `;
+        let historyS =document.getElementById("history");
+        historyS.appendChild(newdiv)
+        arrayHistory.push(element)
+    })
+}
